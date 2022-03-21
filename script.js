f7e4a0ba1d65e0f4b6de4af4aa8b593b5750ab7f
Matthew <mattnfowler@gmail.com>
2022-03-21 20:40:47 +0900

    basic game logic in place

--- script.js
+++ script.js
@@ -9,21 +9,22 @@ function computerPlay() {
   }
   
   function play(playerSelection, computerSelection) {
+    computerSelection = computerPlay();
     if ((computerSelection ==="ROCK" && playerSelection.toUpperCase() ==="ROCK") || (computerSelection ==="PAPER" && playerSelection.toUpperCase() ==="PAPER") || (computerSelection ==="SCISSORS" && playerSelection.toUpperCase() ==="SCISSORS")) {
-      return "Both picked, " + playerSelection +" that's a draw!";
-    } else if (computerSelection === "ROCK" && playerSelction.toUpperCase() == "PAPER") {
-      return "You won! Paper beats Rock!";
-    } else if (computerSelection=== "Rock" && playerSelection.toUpperCase() === "SCISSORS") {
-      return "You lost. Rock beasts Scissors.";
-    } else if (computerSelection == "PAPER" && playerSelection.toUpperCase()=="ROCK") {
-      return "You lost. Paper beats Rock.";
-    } else if (computerSelection== "PAPER" && PlayerSelection.toUpperCase() == "SCISSORS") {
-      return "You won. Scissors beats Paper.";
-    } else {
-      return "still working";
+        return "Both picked, " + playerSelection +" that's a draw!";
+    } else if (computerSelection === "ROCK" && playerSelection.toUpperCase() === "PAPER") {
+        return "You won! Paper beats Rock!";
+    } else if (computerSelection=== "ROCK" && playerSelection.toUpperCase() === "SCISSORS") {
+        return "You lost. Rock beasts Scissors.";
+    } else if (computerSelection === "PAPER" && playerSelection.toUpperCase()==="ROCK") {
+        return "You lost. Paper beats Rock.";
+    } else if (computerSelection=== "PAPER" && playerSelection.toUpperCase() === "SCISSORS") {
+        return "You won. Scissors beats Paper.";
+    } else if (computerSelection === "SCISSORS" && playerSelection.toUpperCase() === "PAPER") {
+        return "You lost. Scissors beasts paper";
+    } else if (computerSelection === "SCISSORS" && playerSelection.toUpperCase() === "ROCK") {
+      return "You win! Rock beats Scissors!";
     }
   }
   
-  const playerSelection = "Rock";
-  const computerSelection = computerPlay();
-  console.log(play(playerSelection, computerSelection));
+  
